8db03c1e97691dfffe9b7b018446814db02efff3
Nivedita <nivedita8631@gmail.com>
2024-10-01 20:24:10 +0530

    Making changes

---
+++ my-todo/src/App.jsx
@@ -0,0 +1,10 @@
+
+import './App.css';
+
+
+function main() {
+<h1>Welcome to my Todo app</h1>
+    
+}
+
+export default App;
---
+++ my-todo/src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function main(){
+<h1> Welcome to my Todo App</h1>
+}
+
+export default main;
